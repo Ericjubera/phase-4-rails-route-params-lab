6a0e6805c01b07d230342fd63c25009299dc6ec6
eric jubera <ericjubera@gmail.com>
2022-10-10 11:40:04 -0500

    commit tree 016263528a79a9c5aedf46545a2d4f44f5ee9c6d

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,8 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
-
+def usk
+  students = Student.find(params[:id])
+  render json: students
+end 
 end
